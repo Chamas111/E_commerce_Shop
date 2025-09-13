cddaefbd7dfa919628c9f93519aa1b00ac39774e
Chamas111 <ahmad.chamas87@gmail.com>
2025-09-14 01:49:19 +0200

    add link for backend

--- frontend/src/Components/axiosInstance.js
+++ frontend/src/Components/axiosInstance.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_SERVER_BASE_URL || "",
+  baseURL:
+    process.env.REACT_APP_SERVER_BASE_URL ||
+    "https://e-commerce-shop-jf35.onrender.com",
 });
 
 export default instance;
